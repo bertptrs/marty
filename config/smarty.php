e2d732efa64e00b7d4889a6e35fecbe93e5f6db5
Bert Peters <bert.ljpeters@gmail.com>
2013-11-18 20:32:40 +0100

    Fix a typo. Comma's...

--- config/smarty.php
+++ config/smarty.php
@@ -3,6 +3,6 @@
 return array(
 	"templateDir" => MAKO_APPLICATION_PATH . "/views",
 	"cacheDir" => MAKO_APPLICATION_PATH . "/storage/smarty/cache",
-	"compileDir" => MAKO_APPLICATION_PATH . "/storage/smarty/compile"
-	"configDir" => MAKO_APPLICATION_PATH . "/storage/smarty/config"
+	"compileDir" => MAKO_APPLICATION_PATH . "/storage/smarty/compile",
+	"configDir" => MAKO_APPLICATION_PATH . "/storage/smarty/config",
 );
