cf9d9e2b55bdbcdf8bd720a6924bf2afb327eb36
Bert Peters <bert.ljpeters@gmail.com>
2015-04-16 12:22:33 +0200

    Version bump.
    Also modified view directory to comply with Mako framework changes.

--- config/smarty.php
+++ config/smarty.php
@@ -3,7 +3,7 @@
 use \Smarty;
 
 return [
-	"templateDir" => MAKO_APPLICATION_PATH . "/views",
+	"templateDir" => MAKO_APPLICATION_PATH . "/resources/views",
 	"compileDir" => MAKO_APPLICATION_PATH . "/storage/smarty/",
 	"pluginDirs" => [
 		"./plugins",
