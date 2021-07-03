41c766b2fcbb401ddae5581907e8f78bd541c26f
Bert Peters <bert@bertptrs.nl>
2021-07-03 21:49:35 +0200

    Bump php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,9 +4,13 @@ $finder = PhpCsFixer\Finder::create()
     ->exclude('vendor')
     ->in(__DIR__);
 
-return PhpCsFixer\Config::create()
+$config = new PhpCsFixer\Config();
+
+$config
     ->setUsingCache(true)
     ->setRules([
         '@PSR2'              => true,
         'single_quote'       => true,
     ])->setFinder($finder);
+
+return $config;
