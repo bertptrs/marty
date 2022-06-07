3d8e3b9ece16dd841e446ad19d9dbee9139cb0d5
Bert Peters <bert@bertptrs.nl>
2022-06-07 23:20:54 +0200

    Fix tests for 7.2

--- tests/resources/plugins/compiler.echo.php
+++ tests/resources/plugins/compiler.echo.php
@@ -3,8 +3,8 @@
 function smarty_compiler_echo()
 {
     return <<<EOD
-        <?php
-        echo 'Yes, compiler functions work!' . PHP_EOL;
-        ?>
-        EOD;
+<?php
+echo 'Yes, compiler functions work!' . PHP_EOL;
+?>
+EOD;
 }
